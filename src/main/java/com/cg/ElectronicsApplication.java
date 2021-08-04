41840d3400cbdab3d193023a1d44d2274d659c33
Tin Tran <79630949+tintran271195@users.noreply.github.com>
2021-08-04 10:24:20 +0700

    merge

--- src/main/java/com/cg/ElectronicsApplication.java
+++ src/main/java/com/cg/ElectronicsApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ElectronicsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElectronicsApplication.class, args);
     }
+
+
+
 }
